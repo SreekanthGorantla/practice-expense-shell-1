869a329f9bf3e67a820cda5d1eff01ebf2db606b
Sreekanth Gorantla <gorantlasreekanth@gmail.com>
2025-05-22 11:12:24 -0500

    practice shell 1

--- mysql.sh
+++ mysql.sh
@@ -17,12 +17,12 @@ N="\e[0m"
 # 
 #########################################################
 VALIDATE(){
-    if [ $1 != 0 ]
+    if [ $1 -ne 0 ]
     then
-    echo -e "$2 ... $R FAILURE $N"
-    exit 1
+        echo -e "$2 ... $R FAILURE $N"
+        exit 1
     else
-    echo -e "$2 ... $G SUCCESS $N"
+        echo -e "$2 ... $G SUCCESS $N"
     fi
 }
 
@@ -34,13 +34,12 @@ LOG_FILE=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
-
 echo "Script started executing at: $TIMESTAMP" &>> $LOG_FILE_NAME
 
-if [ $USERID != 0 ]
+if [ $USERID -ne 0 ]
 then
-echo "Error:: You must have Sudo access to execute this command"
-exit 1
+    echo "Error:: You must have Sudo access to execute this command"
+    exit 1
 fi
 
 for package in @$
@@ -48,9 +47,9 @@ do
     dnf list installed $package &>> $LOG_FILE_NAME
     if [ $? -ne 0 ]
     then
-    dnf install $package -y &>> $LOG_FILE_NAME
-    VALIDATE $? "Installing $package"
+        dnf install $package -y &>> $LOG_FILE_NAME
+        VALIDATE $? "Installing $package"
     else
-    echo -e "$package already installed $Y ... INSTALLED $N"
+        echo -e "$package already installed $Y ... INSTALLED $N"
     fi
 done
