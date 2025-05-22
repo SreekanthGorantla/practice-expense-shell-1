0083b32ac6a53edb967877df8fb0f15f8153a6f8
Sreekanth Gorantla <gorantlasreekanth@gmail.com>
2025-05-22 11:22:23 -0500

    practice shell 1

--- mysql.sh
+++ mysql.sh
@@ -26,6 +26,14 @@ VALIDATE(){
     fi
 }
 
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo "Error:: You must have Sudo access to execute this command"
+        exit 1
+    fi
+}
+
 #########################################################
 # MAIN
 #########################################################
@@ -34,22 +42,34 @@ LOG_FILE=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
+mkdir -p $LOGS_FOLDER
 echo "Script started executing at: $TIMESTAMP" &>> $LOG_FILE_NAME
 
-if [ $USERID -ne 0 ]
-then
-    echo "Error:: You must have Sudo access to execute this command"
-    exit 1
-fi
+CHECK_ROOT
 
-for package in @$
-do
-    dnf list installed $package &>> $LOG_FILE_NAME
-    if [ $? -ne 0 ]
-    then
-        dnf install $package -y &>> $LOG_FILE_NAME
-        VALIDATE $? "Installing $package"
-    else
-        echo -e "$package already installed $Y ... INSTALLED $N"
-    fi
-done
+#########################################################
+# mysql server installation
+#########################################################
+dnf install mysql-server -y &>> $LOG_FILE_NAME
+VALIDATE $? "Installing MySQL Server"
+
+systemctl enable mysqld &>> $LOG_FILE_NAME
+VALIDATE $? "Enabling MySQL Server"
+
+systemctl start mysqld &>> $LOG_FILE_NAME
+VALIDATE $? "Starting MySQL Server"
+
+#########################################################
+# mysql server root password setup
+#########################################################
+
+mysql -h mysql.sreeaws.space -u root -pExpenseApp@1 -e 'show databases;' &>> $LOG_FILE_NAME
+
+if [ $? -ne 0 ]
+then
+    echo "MySQL Root password not setup" &>> $LOG_FILE_NAME
+    mysql_secure_installation --set-root-pass ExpenseApp@1
+    VALIDATE $? "Setting Root Password"
+else
+    echo -e "MySQL Root password already setup ... $Y SKIPPING $N"
+fi
